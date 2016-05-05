f873441cc8bf8744232f4ee149162584cb101d42
Nicolas Chauvet <kwizart@gmail.com>
2016-05-05 09:35:01 +0200

    Add koji roles

---
+++ roles/koji_hub/files/updatecrl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+URL=https://admin.rpmfusion.org/ca/crl.pem
+OLD=/etc/pki/tls/crl.pem
+NEW=`mktemp`
+
+wget $URL -O $NEW
+OLDUPDATE=`openssl crl -in $OLD -noout -lastupdate`
+NEWUPDATE=`openssl crl -in $NEW -noout -lastupdate`
+
+if [ "$OLDUPDATE" != "$NEWUPDATE" ]; then
+    cp -f $NEW $OLD
+    /sbin/restorecon $OLD
+    /etc/init.d/httpd graceful
+    echo "updated to $NEWUPDATE"
+fi
+
+rm -f $NEW
